fc396f00920450052ec11c795ecc1c4f4095d25f
md shahnewaz zaman <mzaman@resqsoft.com>
2023-06-23 16:23:28 -0400

    added validation

--- herokuapp/src/test/java/com/gits/herokuapp/tests/ValidateLandingPageLinks.java
+++ herokuapp/src/test/java/com/gits/herokuapp/tests/ValidateLandingPageLinks.java
@@ -1,10 +1,17 @@
 package com.gits.herokuapp.tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class ValidateLandingPageLinks {
+
+    String expectedUrl = "https://the-internet.herokuapp.com/abtest";
 
     @Test
     public void validateLandingPageLinks() throws InterruptedException {
@@ -13,15 +20,38 @@ public class ValidateLandingPageLinks {
         - Pre-condition: Chrome
         - Mac & Windows
 
-        - Navigate to the URL on the Chrome browser
-        - Validating expected URL
-        - Validate 44 links on the landing page
+        - 1. Navigate to the URL on the Chrome browser
+        - 2. Validating expected URL
+        - Validate links on the landing page
+        - How do I get count of the link on landing page
+        - Validate, the count of the links and expected links from the reequirment
          */
+
+
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
-        Thread.sleep(5000);
+        Thread.sleep(1000);
+
+        //Assert.assertEquals("Actual", expectedUrl);
+
+        WebElement firstLink = driver.findElement(By.cssSelector("li:nth-of-type(1) > a"));
+        firstLink.click();
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, expectedUrl);
+        driver.navigate().back();
+
+        List<WebElement> linkElements = driver.findElements(By.tagName("a"));
+        int linkCount = linkElements.size();
+        Assert.assertEquals(linkCount, 46, "No links found on the webpage.");
+
+
+
+
+
+        Thread.sleep(4000);
         driver.quit();
+
 
     }
 }
