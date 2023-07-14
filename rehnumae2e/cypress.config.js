2a3936d5da3d9f6c724a108d5807119a44f44032
md shahnewaz zaman <mzaman@resqsoft.com>
2023-07-13 22:24:17 -0400

    new student inital cypressIO Repo

---
+++ rehnumae2e/cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://the-internet.herokuapp.com',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ rehnumae2e/cypress/e2e/abTestLink.spec.cy.js
@@ -0,0 +1,26 @@
+describe('abTest cases', () => {
+
+    beforeEach(() => {
+        cy.visit('/')
+      })
+
+    it('validate, A/B Testing text from the home page', () => {
+        cy.get('li:nth-of-type(1) > a')
+            .should('have.attr', 'href', '/abtest'); 
+    })
+
+    it('validate, Add/Remove Elements text from the home page', () => {
+        cy.get('li:nth-of-type(2) > a')
+            .should('have.attr', 'href', '/add_remove_elements/');
+    })
+
+    it('validate, Basic Auth text from the home page', () => {
+        cy.get('li:nth-of-type(3) > a')
+            .should('have.attr', 'href', '/basic_auth');
+    })
+
+    it('validate, Broken Images text from the home page', () => {
+        cy.get('li:nth-of-type(4) > a')
+            .should('have.attr', 'href', '/broken_images');
+    })
+})
